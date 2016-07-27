fb7f551d603ebe3711fde1328f159dfd49240835
Aman <amanpreet.hundal@gmail.com>
2016-07-27 19:38:40 +0000

    timestamp code

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
-var express = require("express");
+var express     = require('express');
+var dateFormat  = require("dateformat");
 
-var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  var result = {unix: null, natural: null};
+  res.send(result);
+  
+});
+
+app.get('/:date', function (req, res) {
+
+  var reqDate = req.params.date;
+  var unix = null;
+  var natural = null;
+
+  if(reqDate.includes(" "))
+  {
+    unix = new Date(reqDate).getTime() / 1000;
+    natural = reqDate;
+  }
+  else
+  {
+    unix = reqDate;
+    natural = dateFormat(reqDate*1000,"mmmm d, yyyy");
+  }
+  
+  var result = {unix: unix, natural: natural};
+  res.send(result);
+  
 });
 
 app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port !'+process.env.PORT);
+  console.log('App listening on port !'+process.env.PORT);
 });
